7e75eac76a8f2ac18da300ec5ad7359c84865f44
William Killerud <william.killerud@schibsted.com>
2024-07-11 09:33:41 +0200

    chore: reconfigure so project runs on Windows

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,22 @@
 import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import js from '@eslint/js';
 
 export default [
     js.configs.recommended,
     prettierConfig,
+    prettierPlugin,
     {
-        plugins: {
-            prettier: prettierPlugin,
-        },
         languageOptions: {
             globals: {
+                ...globals.node,
                 ...globals.browser,
                 global: true,
             },
         },
     },
     {
-        ignores: ['dist/*'],
+        ignores: ['coverage/*', 'dist/*'],
     },
 ];
